940ae98f668021c61bee5ba8e8c215bd7ca020f0
leohsilva7 <leo.h.silva@aluno.senai.br>
2025-06-10 11:29:56 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnadd = document.getElementById("btn-add");
+const tarefa = document.getElementById("tarefa");
+const taskList = document.getElementById("tasklist");
+
+let nome = prompt("Qual o seu nome?");
+titulo.innerHTML = `Lista de tarefa: ${nome}`;
+
+
+btnadd.addEventListener("click", criaTarefa)
+
+function criaTarefa() {
+
+}
