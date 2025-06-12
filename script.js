80e0abb3553f9309149f7d048dfe35cef06196f4
leohsilva7 <leo.h.silva@aluno.senai.br>
2025-06-12 09:00:43 -0300

    final

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ const titulo = document.getElementById('titulo')
 let nome = prompt("Qual o seu nome?")
 if(nome==""){
     titulo.innerHTML = "Lista de tarefa"
+     tarefa.focus();
 }
 else{
-titulo.innerHTML = `Lista de tarefa: ${nome}`;
+    titulo.innerHTML = `Lista de tarefa: ${nome}`;
+     tarefa.focus();
 }
 // Acompanha o evento de clique do botão adicionar tarefa
 btnadd.addEventListener("click", criaTarefa);
@@ -22,34 +24,43 @@ function criaTarefa(){
     listItem.textContent = tarefa.value;
     tasklist.appendChild(listItem);
 
+    if (tarefa.value == "") {
+        alert("digite o nome da tarefa");
+    }
 
-    //Criar o botão de deletar a tarefa
-    const removeButton = document.createElement('button');
-    removeButton.id ="remove";
-    removeButton.textContent ="X"
-    listItem.appendChild(removeButton);
-
-    removeButton.addEventListener("click", function(){
-        tasklist.removeChild(listItem)
-    })
-
-    //Criar o botão de concluir a tarefa
-    const concluirButton = document.createElement('button');
-    concluirButton.id="conclui";
-    concluirButton.textContent="✓"
-    // listItem.appendChild(concluirButton);
+    else {
 
     
-     let buttonsItem = document.createElement('div');
-    buttonsItem.classList.toggle('buttonsItem');
-    listItem.appendChild(buttonsItem)
-    buttonsItem.appendChild(concluirButton);
-    buttonsItem.appendChild(removeButton);
 
-    concluirButton.addEventListener("click",function(){
-        listItem.classList.toggle('completed');
-    })
 
-    tarefa.value = "";
+        //Criar o botão de deletar a tarefa
+        const removeButton = document.createElement('button');
+        removeButton.id = "remove";
+        removeButton.textContent = "X"
+        listItem.appendChild(removeButton);
 
+        removeButton.addEventListener("click", function () {
+            tasklist.removeChild(listItem)
+        })
+
+        //Criar o botão de concluir a tarefa
+        const concluirButton = document.createElement('button');
+        concluirButton.id = "conclui";
+        concluirButton.textContent = "✓"
+        // listItem.appendChild(concluirButton);
+
+    
+        let buttonsItem = document.createElement('div');
+        buttonsItem.classList.toggle('buttonsItem');
+        listItem.appendChild(buttonsItem)
+        buttonsItem.appendChild(concluirButton);
+        buttonsItem.appendChild(removeButton);
+
+        concluirButton.addEventListener("click", function () {
+            listItem.classList.toggle('completed');
+        })
+
+        tarefa.value = "";
+   
+    }
 }
